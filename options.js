6a9ff33a7a6939ca0cd394f10c723e590f6b67fe
Mattias Bowa <matt.bowa@mail.com>
2018-11-24 23:58:30 +1000

    Add files via upload

---
+++ options.js
@@ -0,0 +1,102 @@
+
+var allLinks = [];
+//var visibleLinks = [];
+
+// Display all visible links.
+function showLinks(visibleLinks) {
+  var linksTable = document.getElementById('links');
+  while (linksTable.children.length > 1) {
+    linksTable.removeChild(linksTable.children[linksTable.children.length - 1])
+  }
+  for (var i = 0; i < visibleLinks.length; ++i) {
+    var row = document.createElement('tr');
+    var col0 = document.createElement('td');
+    var col1 = document.createElement('td');
+    var checkbox = document.createElement('input');
+    checkbox.checked = false;
+    checkbox.type = 'checkbox';
+    checkbox.id = 'check' + i;
+    col0.appendChild(checkbox);
+    col1.innerText = visibleLinks[i];
+    col1.style.whiteSpace = 'nowrap';
+    col1.onclick = function() {
+      checkbox.checked = !checkbox.checked;
+    }
+    row.appendChild(col0);
+    row.appendChild(col1);
+    linksTable.appendChild(row);
+  }
+}
+
+// Toggle the checked state of all visible links.
+function toggleAll() {
+  var checked = document.getElementById('toggle_all').checked;
+  for (var i = 0; i < visibleLinks.length; ++i) {
+    document.getElementById('check' + i).checked = checked;
+  }
+}
+
+// Download all visible checked links.
+function downloadCheckedLinks() {
+  for (var i = 0; i < visibleLinks.length; ++i) {
+    if (document.getElementById('check' + i).checked) {
+      chrome.downloads.download({url: visibleLinks[i]},
+                                             function(id) {
+      });
+    }
+  }
+  window.close();
+}
+
+// Re-filter allLinks into visibleLinks and reshow visibleLinks.
+function filterLinks() {
+  var filterValue = document.getElementById('filter').value;
+  if (document.getElementById('regex').checked) {
+    visibleLinks = allLinks.filter(function(link) {
+      return link.match(filterValue);
+    });
+  } else {
+    var terms = filterValue.split(' ');
+    visibleLinks = allLinks.filter(function(link) {
+      for (var termI = 0; termI < terms.length; ++termI) {
+        var term = terms[termI];
+        if (term.length != 0) {
+          var expected = (term[0] != '-');
+          if (!expected) {
+            term = term.substr(1);
+            if (term.length == 0) {
+              continue;
+            }
+          }
+          var found = (-1 !== link.indexOf(term));
+          if (found != expected) {
+            return false;
+          }
+        }
+      }
+      return true;
+    });
+  }
+  showLinks();
+}
+
+
+function ShowList(){
+	var data = RetrieveFromLocalStorage();
+	showLinks(data);
+}
+
+window.onload = function() {
+ // document.getElementById('filter').onkeyup = filterLinks;
+ // document.getElementById('regex').onchange = filterLinks;
+  //document.getElementById('toggle_all').onchange = toggleAll;
+  //document.getElementById('download0').onclick = GetList;
+  //document.getElementById('download1').onclick = Remove;
+  //document.getElementById('download2').onclick = Redder;
+  document.getElementById('download3').onclick = ResetLocalStorageData;
+  document.getElementById('download4').onclick = ShowList;
+  
+ 
+};
+
+
